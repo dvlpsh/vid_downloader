7fb40b0186b2d6cf3effb1ff75959ae1900051a3
dvlpsh <46413383+dvlpsh@users.noreply.github.com>
2019-09-18 13:36:05 +0530

    Update downld.py

--- downld.py
+++ downld.py
@@ -5,6 +5,6 @@ def download():
 	urls = open('vid_list', 'r')
 	urls.read()
 	urls.close()
-	get_vids = os.system("youtube-dl --output '~/Downloads/%(title)s-%(id)s.%(ext)s' -a vid_list --recode-video 'mkv' ")
+	get_vids = os.system("youtube-dl --output '~/Downloads/%(title)s-%(id)s.%(ext)s' -a vid_list --recode-video 'mp4' ")
 	
 download()
